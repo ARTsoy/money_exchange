0f95b4c236c5d05dac24c059acae086ed73f9a22
ARTsoy <arturtsoy1995@gmail.com>
2019-07-05 09:40:31 +0500

    100%

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = function makeExchange(currency)
     // Your code goes here!
     // Return an object containing the minimum currencyer of coins needed to make change
 	var array=[,,,,];
-	var array2=["H: ", "Q: ", "D: ", "N: ","P: "];
 	result=new Object();
 	result={}; 
 	var ind=0;
@@ -21,40 +20,35 @@ module.exports = function makeExchange(currency)
 			else
 			{
 				array[0]=Math.floor(currency/50);
+				if(array[0]>0)
+				{
+					result["H"]=array[0];
+				}
 				currency=currency-array[0]*50;
 				array[1]=Math.floor(currency/25);
+				if(array[1]>0)
+				{
+					result["Q"]=array[1];
+				}
 				currency=currency-array[1]*25;
 				array[2]=Math.floor(currency/10);
+				if(array[2]>0)
+				{
+					result["D"]=array[2];
+				}
 				currency=currency-array[2]*10;
 				array[3]=Math.floor(currency/5);
+				if(array[3]>0)
+				{
+					result["N"]=array[3];
+				}
 				currency=currency-array[3]*5;
 				array[4]=currency;
-				for(var i=0;i<5;i++)
-				{	
-					/*if(i==0)
-					{
-						result = result + "{ ";
-					}*/
-						
-					if(array[i]!=0)
-					{	
-						if(ind>0)
-						{
-							result= result + ", " + array2[i] + array[i]	
-						}
-						else
-						{
-							result= result + array2[i] + array[i];
-						}
-						ind++;
-						
-					}
-					/*if(i==4)
-						{
-							result=result + "}";
-						}*/
-				
+				if(array[4]>0)
+				{
+					result["P"]=array[4];
 				}
+				
 				return result;
 			}
 		}
